c27a6656aeabc57a0ee281c93cafaa81b808af05
Philippe Crama <pcfeb0009@gmx.com>
2017-01-27 12:59:25 +0100

    Profile & optimize EnnGram.hs

--- EnnGram.hs
+++ EnnGram.hs
@@ -41,19 +41,117 @@ instance Eq FullEnnGram where
 
 instance Ord FullEnnGram where
   compare (FullEnnGram s e) (FullEnnGram t f)
+    -- This isn't lexicographically ordering (we compare on length first) but
+    -- any stable/strict ordering will do for Map insertion and looking at the
+    -- length first should be cheaper (actually not measured)
     | sLen > tLen = GT
     | sLen < tLen = LT
-    | sLen == tLen =
-        foldr cpComp EQ $ take (fromIntegral sLen) $ zip [(ennOffs s)..] [(ennOffs t)..]
+    -- See note on profiling below
+    | sLen == tLen = foldr cpComp EQ [0..fromIntegral sLen - 1]
     where sLen = ennLength s
           tLen = ennLength t
-          cpComp (sIdx, tIdx) res =
-              let sCp = e ! sIdx
-                  tCp = f ! tIdx
+          sIdx = ennOffs s
+          tIdx = ennOffs t
+          cpComp offs res =
+              let sCp = e ! (sIdx + offs)
+                  tCp = f ! (tIdx + offs)
                   comp = compare sCp tCp
               in case comp of
                    EQ -> res
                    otherwise -> comp
+
+-- Profiling showed that the original implementation of compare was allocating a lot
+-- I tried different implementations with these results:
+-- [[ Compiling with ghc -O2 -prof -fprof-auto-top Main.hs ]]
+-- [[ Running with Main +RTS -p -RTS ]]
+-- Original foldr:
+-- FLODR1: 	total time  =        0.66 secs   (659 ticks @ 1000 us, 1 processor)
+-- FLODR1: 	total alloc =  31,171,348 bytes  (excludes profiling overheads)
+-- FLODR1: COST CENTRE        MODULE             %time %alloc
+-- FLODR1: compare            EnnGram             51.0   53.8
+-- FLODR1: ennGramMap         EnnGram             25.9   21.9
+-- FLODR1:  instance Ord FullEnnGram where
+-- FLODR1:    compare (FullEnnGram s e) (FullEnnGram t f)
+-- FLODR1:      | sLen > tLen = GT
+-- FLODR1:      | sLen < tLen = LT
+-- FLODR1:      | sLen == tLen =
+-- FLODR1:          foldr cpComp EQ $ take (fromIntegral sLen) $ zip [(ennOffs s)..] [(ennOffs t)..]
+-- FLODR1:      where sLen = ennLength s
+-- FLODR1:            tLen = ennLength t
+-- FLODR1:            cpComp (sIdx, tIdx) res =
+-- FLODR1:                let sCp = e ! sIdx
+-- FLODR1:                    tCp = f ! tIdx
+-- FLODR1:                    comp = compare sCp tCp
+-- FLODR1:                in case comp of
+-- FLODR1:                     EQ -> res
+-- FLODR1:                     otherwise -> comp
+-- With Data.List.foldl'
+-- FOLDL': 	total time  =        2.17 secs   (2169 ticks @ 1000 us, 1 processor)
+-- FOLDL': 	total alloc = 165,408,080 bytes  (excludes profiling overheads)
+-- FOLDL': COST CENTRE        MODULE             %time %alloc
+-- FOLDL': compare            EnnGram             83.9   91.3
+-- FOLDL': ennGramMap         EnnGram              9.3    4.1
+-- FOLDL': makeCandidates     CandidateSelection   2.4    2.3
+-- FOLDL': enqueueNewEnnGrams EnnGram              1.2    0.6
+-- FOLDL':  instance Ord FullEnnGram where
+-- FOLDL':    compare (FullEnnGram s e) (FullEnnGram t f)
+-- FOLDL':      | sLen > tLen = GT
+-- FOLDL':      | sLen < tLen = LT
+-- FOLDL':      | sLen == tLen = foldl' cpComp EQ $ take (fromIntegral sLen) $ zip [(ennOffs s)..] [(ennOffs t)..]
+-- FOLDL':      where sLen = ennLength s
+-- FOLDL':            tLen = ennLength t
+-- FOLDL':            cpComp res (sIdx, tIdx) =
+-- FOLDL':                let sCp = e ! sIdx
+-- FOLDL':                    tCp = f ! tIdx
+-- FOLDL':                    comp = compare sCp tCp
+-- FOLDL':                in case comp of
+-- FOLDL':                     EQ -> res
+-- FOLDL':                     otherwise -> comp
+-- Using direct recursion (to avoid consing up the list of indices to visit)
+-- RECURS: 	total time  =        0.45 secs   (449 ticks @ 1000 us, 1 processor)
+-- RECURS: 	total alloc =  14,414,360 bytes  (excludes profiling overheads)
+-- RECURS: COST CENTRE        MODULE             %time %alloc
+-- RECURS: ennGramMap         EnnGram             32.5   47.4
+-- RECURS: compare            EnnGram             27.4    0.0
+-- RECURS: makeCandidates     CandidateSelection  11.4   25.8
+-- RECURS:  instance Ord FullEnnGram where
+-- RECURS:    compare (FullEnnGram s e) (FullEnnGram t f)
+-- RECURS:      | sLen > tLen = GT
+-- RECURS:      | sLen < tLen = LT
+-- RECURS:      | sLen == tLen = go 0 (ennOffs s) (ennOffs t)
+-- RECURS:      where sLen = ennLength s
+-- RECURS:            tLen = ennLength t
+-- RECURS:            go idx sOffs tOffs | idx >= sLen = EQ
+-- RECURS:                               | otherwise = let sCp = e ! sOffs
+-- RECURS:                                                 tCp = f ! tOffs
+-- RECURS:                                                 comp = compare sCp tCp
+-- RECURS:                                               in case comp of
+-- RECURS:                                                      EQ -> go (idx + 1) (sOffs + 1) (tOffs + 1)
+-- RECURS:                                                      otherwise -> comp
+-- Final version (using foldr again but consing up a smaller structure)
+-- FOLDR2: 	total time  =        0.41 secs   (409 ticks @ 1000 us, 1 processor)
+-- FOLDR2: 	total alloc =  14,414,360 bytes  (excludes profiling overheads)
+-- FOLDR2: COST CENTRE        MODULE             %time %alloc
+-- FOLDR2: ennGramMap         EnnGram             36.4   47.4
+-- FOLDR2: compare            EnnGram             28.1    0.0
+-- FOLDR2: makeCandidates     CandidateSelection  14.4   25.8
+-- FOLDR2: enqueueNewEnnGrams EnnGram              4.9    7.4
+-- FOLDR2:  instance Ord FullEnnGram where
+-- FOLDR2:    compare (FullEnnGram s e) (FullEnnGram t f)
+-- FOLDR2:      | sLen > tLen = GT
+-- FOLDR2:      | sLen < tLen = LT
+-- FOLDR2:      | sLen == tLen = foldr cpComp EQ [0..fromIntegral sLen - 1]
+-- FOLDR2:      where sLen = ennLength s
+-- FOLDR2:            tLen = ennLength t
+-- FOLDR2:            sIdx = ennOffs s
+-- FOLDR2:            tIdx = ennOffs t
+-- FOLDR2:            cpComp offs res =
+-- FOLDR2:                let sCp = e ! (sIdx + offs)
+-- FOLDR2:                    tCp = f ! (tIdx + offs)
+-- FOLDR2:                    comp = compare sCp tCp
+-- FOLDR2:                in case comp of
+-- FOLDR2:                     EQ -> res
+-- FOLDR2:                     otherwise -> comp
 
 instance Show EnnGram where
   show x = "EG " ++ (show $ ennOffs x) ++ ":" ++ (show $ ennLength x)
