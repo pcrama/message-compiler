b4a13ff651a600b8a396c03aef40c5cfc9d45956
Philippe Crama <pcfeb0009@gmx.com>
2017-01-27 12:58:07 +0100

    Remove unused (test) top level forms

--- Main.hs
+++ Main.hs
@@ -14,14 +14,6 @@ import EnnGram
 import InputText
 import CandidateSelection
 
--- Test case: stringList = ["ababa", "aba", "tab", "aba"]
--- -> at one time during development, the output included an
--- EnnGram "ba\0" (or "\0ba"?), i.e. with a word separator
-stringList = ["ababa", "aba", "tab", "aba"]
-(Just txt) = toCodepoints stringList
-
-(mmpp, ddtt) = ennGramMap txt
-
 readLicense :: IO InputText
 readLicense = do
   theText <- readFile "LICENSE"
